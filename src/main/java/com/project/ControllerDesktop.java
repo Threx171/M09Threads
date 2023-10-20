48e77ed29f0f3abf18bd27bfb320a9deed86a3fa
LeoAlendro171 <118751279+LeoAlendro171@users.noreply.github.com>
2023-10-20 17:04:10 +0200

    intentando

--- src/main/java/com/project/ControllerDesktop.java
+++ src/main/java/com/project/ControllerDesktop.java
@@ -5,6 +5,10 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.text.Text;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 public class ControllerDesktop {
     @FXML
     private ProgressBar progressbar1, progressbar2, progressbar3;
@@ -15,4 +19,12 @@ public class ControllerDesktop {
     @FXML
     private Button btnTask1, btnTask2, btnTask3;
 
+    private ExecutorService executor = Executors.newSingleThreadExecutor();
+
+    public Future<Integer> countByOne (Integer input) {
+        return executor.submit(() -> {
+            Thread.sleep(1000);
+            return input + 1;
+        });
+    }
 }
