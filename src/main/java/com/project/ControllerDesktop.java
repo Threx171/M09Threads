182d67a1e7bf563eef702156bee30f6661411b53
LeoAlendro171 <118751279+LeoAlendro171@users.noreply.github.com>
2023-10-25 20:51:32 +0200

    ganas de gritar disminuyendo

--- src/main/java/com/project/ControllerDesktop.java
+++ src/main/java/com/project/ControllerDesktop.java
@@ -1,8 +1,6 @@
 package com.project;
 
 import javafx.application.Platform;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ProgressBar;
@@ -11,6 +9,7 @@ import javafx.scene.text.Text;
 import java.util.Random;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class ControllerDesktop {
     @FXML
@@ -23,7 +22,9 @@ public class ControllerDesktop {
     private Button btnTask1, btnTask2, btnTask3;
 
     private Random random = new Random();
-    private int value1 = 0, value2 = 0, value3 = 0;
+    private AtomicInteger value1 = new AtomicInteger(0);
+    private AtomicInteger value2 = new AtomicInteger(0);
+    private AtomicInteger value3 = new AtomicInteger(0);
     private AtomicBoolean run1 = new AtomicBoolean(false);
     private AtomicBoolean run2 = new AtomicBoolean(false);
     private AtomicBoolean run3 = new AtomicBoolean(false);
@@ -34,14 +35,12 @@ public class ControllerDesktop {
             toggleTask(btnTask1, run1);
             CompletableFuture<Void> pbar1 = CompletableFuture.runAsync(() -> {
                 try {
-                    while(value1 < 100 && run1.get()) {
+                    while(value1.get() < 100 && run1.get()) {
                         Thread.sleep(1000);
-                        value1++;
-                        int finalvalue = value1;
+                        value1.incrementAndGet();
+                        AtomicInteger finalValue = value1;
                         Platform.runLater(() -> {
-                            updatePbar(progressbar1, finalvalue);
-                            txtTask1.setText("Tasca 1" + ": " + finalvalue + "%");
-                            if (finalvalue == 100) { btnTask1.setText("Iniciar"); run1.set(false); value1 = 0; }
+                            updatePbar(progressbar1, finalValue, txtTask1, btnTask1, run1, value1, 1);
                         });
                     }
                 } catch (InterruptedException ex) {
@@ -53,15 +52,13 @@ public class ControllerDesktop {
             toggleTask(btnTask2, run2);
             CompletableFuture<Void> pbar2 = CompletableFuture.runAsync(() -> {
                 try {
-                    while(value2 < 100 && run2.get()) {
+                    while(value2.get() < 100 && run2.get()) {
                         Thread.sleep(random.nextInt(3000,5000));
-                        value2 += random.nextInt(2,4);
-                        if (value2 > 100) { value2 = 100; }
-                        int finalValue = value2;
+                        value2.set(value2.get()+random.nextInt(2,4));
+                        if (value2.get() > 100) { value2.set(100);  }
+                        AtomicInteger finalValue = value2;
                         Platform.runLater(() -> {
-                           updatePbar(progressbar2, finalValue);
-                           txtTask2.setText("Tasca 2" + ": " + finalValue + "%");
-                           if (finalValue == 100) { btnTask2.setText("Iniciar"); run2.set(false); value2 = 0; }
+                            updatePbar(progressbar2, finalValue, txtTask2, btnTask2, run2, value2, 2);
                         });
 
                     }
@@ -74,15 +71,13 @@ public class ControllerDesktop {
             toggleTask(btnTask3, run3);
             CompletableFuture<Void> pbar3 = CompletableFuture.runAsync(() -> {
                 try {
-                    while (value3 < 100 && run3.get()) {
+                    while (value3.get() < 100 && run3.get()) {
                         Thread.sleep(random.nextInt(3000, 8000));
-                        value3 += random.nextInt(4, 6);
-                        if (value3 > 100) { value3 = 100; }
-                        int finalValue = value3;
+                        value3.set(value3.get() + random.nextInt(4, 6));
+                        if (value3.get() > 100) { value3.set(100); }
+                        AtomicInteger finalValue = value3;
                         Platform.runLater(() -> {
-                            updatePbar(progressbar3, finalValue);
-                            txtTask3.setText("Tasca 3" + ": " + finalValue + "%");
-                            if (finalValue == 100) { btnTask3.setText("Iniciar"); run3.set(false); value3 = 0; }
+                            updatePbar(progressbar3, finalValue, txtTask3, btnTask3, run3, value3, 3);
                         });
                     }
                 } catch (InterruptedException ex) {
@@ -91,8 +86,11 @@ public class ControllerDesktop {
             });
         });
     }
-    public void updatePbar(ProgressBar pbar, int value) {
-        pbar.setProgress((double) value / 100);
+    public void updatePbar(ProgressBar pbar, AtomicInteger finalValue, Text txt, Button btn, AtomicBoolean isRunning,
+                           AtomicInteger currentValue, int taskNumber) {
+        pbar.setProgress((double) finalValue.get() / 100);
+        txt.setText(String.format("Tasca %d: %d%%", taskNumber, finalValue.get()));
+        if (finalValue.get() == 100) { btn.setText("Iniciar"); isRunning.set(false); currentValue.set(0); }
     }
     public void toggleTask(Button btn, AtomicBoolean isRunning) {
         if (isRunning.get()) {
@@ -103,6 +101,4 @@ public class ControllerDesktop {
             btn.setText("Aturar");
         }
     }
-
-
 }
