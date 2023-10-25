ce4fb826a0685a7a9fc9f4ae94e2276fd89d351e
LeoAlendro171 <118751279+LeoAlendro171@users.noreply.github.com>
2023-10-25 21:11:27 +0200

    codi rehecho con executor y newFixedThreadPool

--- src/main/java/com/project/ControllerDesktop.java
+++ src/main/java/com/project/ControllerDesktop.java
@@ -7,7 +7,8 @@ import javafx.scene.control.ProgressBar;
 import javafx.scene.text.Text;
 
 import java.util.Random;
-import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -29,76 +30,69 @@ public class ControllerDesktop {
     private AtomicBoolean run2 = new AtomicBoolean(false);
     private AtomicBoolean run3 = new AtomicBoolean(false);
 
+    private ExecutorService executor = Executors.newFixedThreadPool(3);
 
     public void initialize() {
-        btnTask1.setOnAction(e -> {
-            toggleTask(btnTask1, run1);
-            CompletableFuture<Void> pbar1 = CompletableFuture.runAsync(() -> {
-                try {
-                    while(value1.get() < 100 && run1.get()) {
-                        Thread.sleep(1000);
-                        value1.incrementAndGet();
-                        AtomicInteger finalValue = value1;
-                        Platform.runLater(() -> {
-                            updatePbar(progressbar1, finalValue, txtTask1, btnTask1, run1, value1, 1);
-                        });
-                    }
-                } catch (InterruptedException ex) {
-                    throw new RuntimeException(ex);
-                }
-            });
-        });
-        btnTask2.setOnAction(e -> {
-            toggleTask(btnTask2, run2);
-            CompletableFuture<Void> pbar2 = CompletableFuture.runAsync(() -> {
-                try {
-                    while(value2.get() < 100 && run2.get()) {
-                        Thread.sleep(random.nextInt(3000,5000));
-                        value2.set(value2.get()+random.nextInt(2,4));
-                        if (value2.get() > 100) { value2.set(100);  }
-                        AtomicInteger finalValue = value2;
-                        Platform.runLater(() -> {
-                            updatePbar(progressbar2, finalValue, txtTask2, btnTask2, run2, value2, 2);
-                        });
+        btnTask1.setOnAction(e -> runTask(1, value1, run1, progressbar1, txtTask1, btnTask1));
+        btnTask2.setOnAction(e -> runTask(2, value2, run2, progressbar2, txtTask2, btnTask2));
+        btnTask3.setOnAction(e -> runTask(3, value3, run3, progressbar3, txtTask3, btnTask3));
+    }
 
-                    }
-                } catch (InterruptedException ex) {
-                    throw new RuntimeException(ex);
-                }
-            });
-        });
-        btnTask3.setOnAction(e -> {
-            toggleTask(btnTask3, run3);
-            CompletableFuture<Void> pbar3 = CompletableFuture.runAsync(() -> {
-                try {
-                    while (value3.get() < 100 && run3.get()) {
-                        Thread.sleep(random.nextInt(3000, 8000));
-                        value3.set(value3.get() + random.nextInt(4, 6));
-                        if (value3.get() > 100) { value3.set(100); }
-                        AtomicInteger finalValue = value3;
-                        Platform.runLater(() -> {
-                            updatePbar(progressbar3, finalValue, txtTask3, btnTask3, run3, value3, 3);
-                        });
-                    }
-                } catch (InterruptedException ex) {
-                    throw new RuntimeException(ex);
-                }
-            });
-        });
-    }
-    public void updatePbar(ProgressBar pbar, AtomicInteger finalValue, Text txt, Button btn, AtomicBoolean isRunning,
-                           AtomicInteger currentValue, int taskNumber) {
-        pbar.setProgress((double) finalValue.get() / 100);
-        txt.setText(String.format("Tasca %d: %d%%", taskNumber, finalValue.get()));
-        if (finalValue.get() == 100) { btn.setText("Iniciar"); isRunning.set(false); currentValue.set(0); }
-    }
-    public void toggleTask(Button btn, AtomicBoolean isRunning) {
+    public void runTask(int taskNumber, AtomicInteger currentValue, AtomicBoolean isRunning,
+                        ProgressBar progressBar, Text txt, Button btn) {
         if (isRunning.get()) {
             isRunning.set(false);
             btn.setText("Iniciar");
-        }else{
+        } else {
             isRunning.set(true);
             btn.setText("Aturar");
+            executor.submit(() -> {
+                try {
+                    while (currentValue.get() < 100 && isRunning.get()) {
+                        Thread.sleep(getSleepDuration(taskNumber));
+                        int increment = getIncrement(taskNumber);
+                        int newValue = currentValue.addAndGet(increment);
+                        if (newValue > 100) {
+                            newValue = 100;
+                        }
+                        int finalValue = newValue;
+                        Platform.runLater(() -> updateUI(progressBar, txt, btn, isRunning, currentValue, finalValue, taskNumber));
+                    }
+                } catch (InterruptedException ex) {
+                    throw new RuntimeException(ex);
+                }
+            });
+        }
+    }
+
+    private long getSleepDuration(int taskNumber) {
+        if (taskNumber == 1) {
+            return 1000;
+        } else if (taskNumber == 2) {
+            return random.nextInt(3000, 5000);
+        } else {
+            return random.nextInt(3000, 8000);
+        }
+    }
+
+    private int getIncrement(int taskNumber) {
+        if (taskNumber == 1) {
+            return 1;
+        } else if (taskNumber == 2) {
+            return random.nextInt(2, 4);
+        } else {
+            return random.nextInt(4, 6);
+        }
+    }
+
+    public void updateUI(ProgressBar progressBar, Text txt, Button btn, AtomicBoolean isRunning, AtomicInteger currentValue,
+                         int finalValue, int taskNumber) {
+        progressBar.setProgress((double) finalValue / 100);
+        txt.setText(String.format("Tasca %d: %d%%", taskNumber, finalValue));
+        if (finalValue == 100) {
+            btn.setText("Iniciar");
+            isRunning.set(false);
+            currentValue.set(0);
         }
     }
 }
